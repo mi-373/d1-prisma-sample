fbbc2b1f0cd821bb58d06b33391f8cf19281ab14
s.minami <sky.sight50@gmail.com>
2024-05-17 17:40:17 +0900

    Add d1 with prisma sample

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,20 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { PrismaClient } from '@prisma/client';
+import { PrismaD1 } from '@prisma/adapter-d1';
+
+export interface Env {
+	DB: D1Database;
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const adapter = new PrismaD1(env.DB);
+		const prisma = new PrismaClient({ adapter });
+
+		const users = await prisma.user.findMany();
+		const result = JSON.stringify(users);
+		return new Response(result);
 	},
-};
+} satisfies ExportedHandler<Env>;
